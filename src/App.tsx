6987072ce48c99f44e0c79ca5167134d8d1dd091
Isabela Cristina <isabeladigitalcircle@gmail.com>
2025-08-14 13:37:01 -0300

    feat: implement Logo component and integrate it into the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import "./styles/global.css";
 import { Container } from "./components/Container";
 import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+
+import "./styles/global.css";
+import "./styles/themes.css";
+
 export function App() {
   return (
     <>
+      <Container>
+        <Logo />
+      </Container>
       <Container>
         <Heading>LOGO</Heading>
       </Container>
