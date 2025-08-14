559c1ee6006aa59699a9956eff131b5fd3b33ad2
Isabela Cristina <isabeladigitalcircle@gmail.com>
2025-08-14 14:26:58 -0300

    feat: add DefaultInput component and integrate it into the App form

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Container } from "./components/Container";
 import { Menu } from "./components/Menu";
 import { Logo } from "./components/Logo";
 import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
 
 import "./styles/global.css";
 import "./styles/themes.css";
@@ -17,6 +18,28 @@ export function App() {
       </Container>
       <Container>
         <CountDown />
+      </Container>
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+              labelText="Nome da atividade"
+              id="meuInput"
+              type="text"
+              placeholder="Digite algo"
+            />
+          </div>
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+          <div className="formRow">
+            <button>enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
