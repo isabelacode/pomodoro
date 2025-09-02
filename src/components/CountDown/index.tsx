8c0cea0e9ac251dca580f83e856f625c40ec428c
Isabela Cristina <isabeladigitalcircle@gmail.com>
2025-09-02 14:58:49 -0300

    feat: pass state and setState props to CountDown and MainForm components

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,5 +1,8 @@
 import styles from "./styles.module.css";
+import type { HomeProps } from '../../templates/pages/Home';
 
-export function CountDown() {
-  return <div className={styles.container}>00:00</div>;
+export function CountDown({ state }: HomeProps) {
+  return (
+    <div className={styles.container}>{state.formattedTimeRemaining}</div>
+  );
 }
--- src/templates/pages/Home/index.tsx
+++ src/templates/pages/Home/index.tsx
@@ -5,22 +5,21 @@ import { MainForm } from "../../../components/MainForm";
 import type { TaskStateModel } from "../../../models/TaskStateModel";
 
 
-type HomeProps = {
+export type HomeProps = {
   state: TaskStateModel;
   setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
 };
 
 export function Home(props: HomeProps) {
-  const { state, setState } = props;
 
   return (
     <MainTemplate>
       <Container>
-        <CountDown />
+        <CountDown {...props} />
       </Container>
 
       <Container>
-        <MainForm />
+        <MainForm  {...props} />
       </Container>
     </MainTemplate>
   );
