145e21eaaa651fd10f113207fc73f2b946d84cbb
Isabela Cristina <isabeladigitalcircle@gmail.com>
2025-09-02 14:36:06 -0300

    feat: implement state management in App component and pass state to Home component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,38 @@ import { Home } from "./templates/pages/Home";
 
 import "./styles/global.css";
 import "./styles/themes.css";
+import { useState } from "react";
+import type { TaskStateModel } from "./models/TaskStateModel";
+
+
+// export type TaskStateModel = {
+//   tasks: TaskModel[];
+//   secondsRemaining: number;
+//   formattedSecondsRemaining: string;
+//   activeTask: TaskModel | null;
+//   currentCycle: number; // 1 a 8
+//   config: {
+//     workTime: number;
+//     shortBreakTime: number;
+//     longBreakTime: number;
+//   };
+// };
+
+
+const initialState: TaskStateModel = {
+  task: [],
+  secondsRemaining: 0,
+  formattedTimeRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState)
+  return <Home state={state} setState={setState} />;
 }
