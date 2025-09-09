9d561af6403c067aa8fb30fef3226f0911cec97d
Isabela Cristina <isabeladigitalcircle@gmail.com>
2025-09-09 10:42:57 -0300

    feat: add routing with MainRouter and implement AboutPomodoro and NotFound pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import "./styles/global.css";
 import "./styles/themes.css";
 
-import { Home } from "./templates/pages/Home";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { MessagesContainer } from "./components/MessagesContainer";
+import { MainRouter } from "./routers/MainRouter";
+
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <Home />
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
